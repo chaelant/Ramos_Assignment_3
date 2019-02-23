1561156260914f313bfaa1d4832484850b2a6aa9
Michael <mikeramos92@gmail.com>
2019-02-23 15:31:15 -0500

    cleaned up

--- data/products.js
+++ data/products.js
@@ -9,6 +9,60 @@ module.exports = {
                 'ShortDesc': 'This is product 1',
                 'FullDesc': 'This is all about product 1',
                 'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product2': {
+                'Name': 'Product 2',
+                'ShortDesc': 'This is product 2',
+                'FullDesc': 'This is all about product 2',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product3': {
+                'Name': 'Product 3',
+                'ShortDesc': 'This is product 3',
+                'FullDesc': 'This is all about product 3',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product4': {
+                'Name': 'Product 4',
+                'ShortDesc': 'This is product 4',
+                'FullDesc': 'This is all about product 4',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product5': {
+                'Name': 'Product 5',
+                'ShortDesc': 'This is product 5',
+                'FullDesc': 'This is all about product 5',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product6': {
+                'Name': 'Product 6',
+                'ShortDesc': 'This is product 6',
+                'FullDesc': 'This is all about product 6',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product7': {
+                'Name': 'Product 7',
+                'ShortDesc': 'This is product 7',
+                'FullDesc': 'This is all about product 7',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product8': {
+                'Name': 'Product 8',
+                'ShortDesc': 'This is product 8',
+                'FullDesc': 'This is all about product 8',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product9': {
+                'Name': 'Product 9',
+                'ShortDesc': 'This is product 9',
+                'FullDesc': 'This is all about product 9',
+                'Image': path.resolve('/public/images/product1.jpg')
+            },
+            'Product10': {
+                'Name': 'Product 10',
+                'ShortDesc': 'This is product 10',
+                'FullDesc': 'This is all about product 10',
+                'Image': path.resolve('/public/images/product1.jpg')
             }
         };
     }
--- routes/main.js
+++ routes/main.js
@@ -4,14 +4,9 @@ const path = require('path');
 const products = require('../data/products');
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
     const prodList = products.getProducts();
     try {
         res.render('catalog/catalog', {'ProductList': prodList})
-=======
-    try {
-        res.render('catalog/catalog', {})
->>>>>>> c405f3243173b8f36d1ebe841ca95949c15f85d0
     } catch (e) {
         res.json({error: e});
     }
